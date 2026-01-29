e7108857fbc3333b3fbd888c1805c04001d54c02
Joel Veloz <joevpazm@espol.edu.ec>
2026-01-29 13:11:33 -0500

    fix

--- lib/authClient.ts
+++ lib/authClient.ts
@@ -1,7 +1,11 @@
 import { createAuthClient } from "better-auth/react";
+// import { oauthProviderClient } from "@better-auth/oauth-provider/client";
 
 export const authClient = createAuthClient(
     {
         baseURL: process.env.BASE_URL,
+        // plugins: [
+        //     oauthProviderClient()
+        // ]
     }
 );
